59615c80316ec25e64d4d631cf13c3254389b35c
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2022-03-21 15:53:22 +0000

    Added logos to my links.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <html dir=ltr lang=en><meta charset=utf-8>
+<title>Kierancrossland.xyz</title>
+<style>a:link{color:#458588}a:visited{color:#458588}a:active{color:#458588}a:hover{color:Turquoise;}a{text-decoration:none}body{margin-top:0em;color:#c2658f;background-color:#242424;max-width:60em;padding:0 5.62em;font:1.2em/1.62 sans-serif}h2,h3{line-height:1.2}p{font-size:15px}</style>
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+
+</head>
+<body style=font-size:25px>
+<div>
+ <div>
+ <a href="index.html">/index/</a><a>   </a><a href="home.html">/home/</a><a>  </a><a href="info.html">/info/</a><a>   </a>
+ </a><div><a>
+
+ </div>
+ <br>
+ <h2><em>Welcome.</em></h2>
+ <p style=maargin-top:-1em>   </p>
+
+
+ </div>
+</div>
+<br>
